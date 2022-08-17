3db18f90e5436deb947b34c04046933d0361efb2
vero-sari <veronica.sari.est@tecazuay.edu.ec>
2022-08-17 10:33:49 -0500

    primero

---
+++ src/main/java/com/example/Cursos/Models/Planificacion.java
@@ -0,0 +1,88 @@
+package com.example.Cursos.Models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="planificacion")
+public class Planificacion {
+    private String id;
+    private String nombre_curso;
+    private String docente;
+    private Date fecha_inicio;
+    private Date fecha_fin;
+    private String horario;
+
+    public Planificacion() {
+    }
+
+    
+    
+    public Planificacion(String id, String nombre_curso, String docente, Date fecha_inicio, Date fecha_fin,
+            String horario) {
+        this.id = id;
+        this.nombre_curso = nombre_curso;
+        this.docente = docente;
+        this.fecha_inicio = fecha_inicio;
+        this.fecha_fin = fecha_fin;
+        this.horario = horario;
+    }
+
+
+
+    public Planificacion(String nombre_curso, String docente, Date fecha_inicio, Date fecha_fin, String horario) {
+        this.nombre_curso = nombre_curso;
+        this.docente = docente;
+        this.fecha_inicio = fecha_inicio;
+        this.fecha_fin = fecha_fin;
+        this.horario = horario;
+    }
+
+
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNombre_curso() {
+        return nombre_curso;
+    }
+    public void setNombre_curso(String nombre_curso) {
+        this.nombre_curso = nombre_curso;
+    }
+    public String getDocente() {
+        return docente;
+    }
+    public void setDocente(String docente) {
+        this.docente = docente;
+    }
+    public Date getFecha_inicio() {
+        return fecha_inicio;
+    }
+    public void setFecha_inicio(Date fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
+    public Date getFecha_fin() {
+        return fecha_fin;
+    }
+    public void setFecha_fin(Date fecha_fin) {
+        this.fecha_fin = fecha_fin;
+    }
+    public String getHorario() {
+        return horario;
+    }
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+ 
+
+    
+
+
+
+
+}
